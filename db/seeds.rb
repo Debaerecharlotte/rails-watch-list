9ac31518137478a746059955e6e8d5c78a21df71
Charlotte De Baere <dbr.charlotte@gmail.com>
2023-11-16 15:25:13 +0000

    List controller actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'open-uri'
+require 'json'
+
+Movie.destroy_all
+
+url = 'https://tmdb.lewagon.com/movie/top_rated?language=en-US'
+
+URI.open(url) do |f|
+  f.each_line do |line|
+    movie = JSON.parse(line)
+    movie['results'].each do |m|
+      title = m['title']
+      overview = m['overview']
+      poster_url = m['poster_path']
+      rating = m['vote_average']
+      Movie.create!(title: title, overview: overview, poster_url: poster_url, rating: rating)
+    end
+  end
+end
+
+List.create!(name: 'Classics')
+List.create!(name: 'Fantasy')
+List.create!(name: 'History')
